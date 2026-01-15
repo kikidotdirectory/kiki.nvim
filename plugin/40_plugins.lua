96ab515525dd0b2e0f52884d607ffa5641cdba3a
kiki <142045045+calciferchang@users.noreply.github.com>
2026-01-14 23:17:47 -0500

    set up other LSPs

--- plugin/40_plugins.lua
+++ plugin/40_plugins.lua
@@ -81,7 +81,10 @@ now_if_args(function()
   -- Use `:h vim.lsp.enable()` to automatically enable language server based on
   -- the rules provided by 'nvim-lspconfig'.
   -- Use `:h vim.lsp.config()` or 'after/lsp/' directory to configure servers.
-  vim.lsp.enable({ 'vtsls' })
+  vim.lsp.enable({ 'vtsls',
+                   'astro',
+                   'cssls', 'css_variables',
+                   'superhtml', 'emmet_language_server' })
 end)
 
 -- Formatting =================================================================
