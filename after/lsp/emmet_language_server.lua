758cfb11d4a2daaf43bd3234a7ac047529711513
kiki <142045045+calciferchang@users.noreply.github.com>
2026-02-20 02:21:57 -0500

    configure emmet_language_server

---
+++ after/lsp/emmet_language_server.lua
@@ -0,0 +1,4 @@
+return {
+  -- enable emmet in njk files
+  filetypes = { "css", "eruby", "html", "markdown", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact", "njk", "html.jinja" },
+}
