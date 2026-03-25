dbf800e25676023d2ad06b498ec3e2f2d9750507
kiki <142045045+calciferchang@users.noreply.github.com>
2026-03-25 03:28:11 -0400

    lsp configuration for nunjucks

--- after/lsp/tailwindcss.lua
+++ after/lsp/tailwindcss.lua
@@ -1,9 +1,26 @@
 return {
+	filetypes = {
+		'html.jinja',
+		'jinja',
+		-- defaults below
+		'aspnetcorerazor', 'astro', 'astro-markdown', 'blade', 'clojure',
+		'django-html', 'htmldjango', 'edge', 'eelixir', 'elixir', 'ejs',
+		'erb', 'eruby', 'gohtml', 'gohtmltmpl', 'haml', 'handlebars', 'hbs',
+		'html', 'htmlangular', 'html-eex', 'heex', 'jade', 'leaf', 'liquid',
+		'markdown', 'mdx', 'mustache', 'njk', 'nunjucks', 'php', 'razor',
+		'slim', 'twig', 'css', 'less', 'postcss', 'sass', 'scss', 'stylus',
+		'sugarss', 'javascript', 'javascriptreact', 'reason', 'rescript',
+		'typescript', 'typescriptreact', 'vue', 'svelte', 'templ',
+	},
 	settings = {
-		includeLanguages = {
-			nunjucks = "html",
-			jinja = "html",
-			njk = "html"
+		tailwindCSS = {
+			includeLanguages = {
+				htmldjango = "html",
+				["html.jinja"] = "html",
+				nunjucks = "html",
+				jinja = "html",
+				njk = "html",
+			}
 		}
 	}
 }
